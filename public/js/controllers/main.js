2b7393b97d0519210038afc2493cb99ebcbe65cd
DevoNoel <devon.noeldetilly@gmail.com>
2014-10-28 22:39:01 -0400

    User checkout.js instead of custon form

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,11 +2,24 @@ angular.module('mainController', [])
   .controller('MainCtrl', ['MainSrvc', '$scope', '$http', function(MainSrvc, $scope, $http) {
     $scope.formData = {};
 
-    $scope.showForm = false;
+    var handler = StripeCheckout.configure({
+      key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+      image: 'images/christmas-reverse.png',
+      token: function(token) {
+        // Use the token to create the charge with a server-side script.
+        // You can access the token ID with `token.id`
+      }
+    });
 
-    $scope.toggleForm = function() {
-      $scope.showForm = !$scope.showForm
-    };
+    document.getElementById('contribute-button').addEventListener('click', function(e) {
+      // Open Checkout with further options
+      handler.open({
+        name: 'Christmas Donation',
+        description: 'Contribution to the NCRF',
+        amount: $scope.formData.amount * 100
+      });
+      e.preventDefault();
+    });
 
     $scope.createDonation = function() {
       if (!$.isEmptyObject($scope.formData)) {
