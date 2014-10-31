eb1373116e50bb7209373928a3314dbc196c3d4d
DevoNoel <devon.noeldetilly@gmail.com>
2014-10-31 14:58:57 -0400

    Finalize dependencies, add postinstall script

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-// modules =================================================
-var express = require('express');
-var app     = express();
-var mongoose= require('mongoose');
-var morgan = require('morgan');                   // log to console
-var bodyParser = require('body-parser'); 	        // pull information from HTML POST
-var methodOverride = require('method-override');  // simulate DELETE and PUT
+// modules ====================================================
+var express     = require('express');
+var app         = express();
+var mongoose    = require('mongoose');
+var morgan      = require('morgan');
+var bodyParser  = require('body-parser');
 
-// configuration ===========================================
+// configuration ==============================================
 var db = require('./config/db');
 mongoose.connect(db.url);
 
@@ -15,14 +14,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride());
 
 var stripe = require('stripe')(process.env.STRIPE_SECRET);
 
-// routes ==================================================
+// routes =====================================================
 require('./app/routes.js')(app);
 
-// start server ============================================
+// start server ===============================================
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log("Magic happens on port " + port);
