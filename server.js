e41a9affa67265846ff7ab54e35c4b4aae029e75
DevoNoel <devon.noeldetilly@gmail.com>
2014-11-05 15:33:06 -0500

    HTTPS time

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // modules ====================================================
 var express     = require('express');
 var app         = express();
+var fs          = require('fs');
+var https       = require('https');
 var mongoose    = require('mongoose');
 var morgan      = require('morgan');
 var bodyParser  = require('body-parser');
@@ -20,7 +22,13 @@ var stripe = require('stripe')(process.env.STRIPE_SECRET);
 // routes =====================================================
 require('./app/routes.js')(app);
 
+// configure ssl certs ========================================
+var options = {
+  key: fs.readFileSync(process.env.SSL_KEY || 'test/certs/key.pem'),
+  cert: fs.readFileSync(process.env.SSL_CERT || 'test/certs/cert.pem')
+};
+
 // start server ===============================================
 var port = process.env.PORT || 5000;
-app.listen(port);
+https.createServer(options, app).listen(port);
 console.log("Magic happens on port " + port);
